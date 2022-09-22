ad94c2c5c88f09cf8c2abb379b0a707a7ae44b1a
Igor Pankevich <pankevich@gmail.com>
2022-09-22 17:16:32 -0400

    added a var for s3 state bucket

--- variables.tf
+++ variables.tf
@@ -1,5 +1,15 @@
+# AWS S3-based remote state storage
+#
+variable "tf_state_s3_bucket_name" {
+  description = "Name of the AWS S3 bucket containing our Terraform state"
+  type        = string
+  default     = "pankusha-tf-state"
+}
+
+# Infrastructure variables
+#
 variable "instance_name" {
   description = "Value of the Name Tag gor the EC2 instance"
   type        = string
-  default     = "EbuchayaHuinya"
+  default     = "EbuchayaHuinya2"
 }
