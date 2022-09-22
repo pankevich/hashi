6fc0bc6d5aa7392964c26deba02458379ae922bd
Igor Pankevich <pankevich@gmail.com>
2022-09-22 17:14:34 -0400

    State migrated to terraform cloud

--- main.tf
+++ main.tf
@@ -1,12 +1,29 @@
 terraform {
+
+#   backend "s3" {
+#     bucket         = "pankusha-tf-state"
+#     key            = "tf-infra/hashi_tf.state"
+#     region         = "us-east-1"
+#     dynamodb_table = "terraform-state-locking"
+#     encrypt        = true
+#   }
+# :3,9s/^/# /   For local backend
+# :3,9s/^# //   For s3-based backend
+#
+
+  cloud {
+    organization = "Pankusha"
+    workspaces {
+      name = "hashi"
+    }
+  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 4.16"
     }
   }
-
-  required_version = ">= 1.2.0"
+  required_version = ">= 1.3.0"
 }
 
 provider "aws" {
@@ -19,7 +36,6 @@ resource "aws_instance" "app_server" {
   instance_type = "t2.micro"
 
   tags = {
-    Name = "ExampleAppServerInstance"
+    Name = var.instance_name
   }
 }
-
